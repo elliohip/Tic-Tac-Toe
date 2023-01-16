47c20c298c769a041a870377bbdb41536f3e153e
Elliot Hipski <elhip03@gmail.com>
2023-01-16 17:19:11 -0500

    restart, different design

--- script.js
+++ script.js
@@ -1,205 +1,147 @@
-const x_character = document.getElementById("x-character");
-const o_character = document.getElementById("o-character");
+let player = (data) => {
+    return {
+    playerChar: data,
+    setPlayer(d) {
+        this.playerChar = d
+    },
 
-const start_button = document.getElementById("start-button");
-
-let playerCharacter;
-let computerCharacter;
-
-function makePlayers(e) {
-    playerCharacter = e.target.innerHTML;
     
-    if (playerCharacter.toLowerCase() == 'x') {
-        computerCharacter = 'o';
+    move(e) {
+        e.target.innerHTML = this.playerChar;
+
+        
+    },
+
+    
+    computerMove(board) {
+
+        let rows = board.rows();
+        let columns = board.columns();
     }
-    else {
-        computerCharacter = 'x';
-    }
-}
-
-
-
-
-class Player {
-    constructor() {
-        this.data = undefined;
-    }
-    constructor(d) {
-        this.data = d;
-    }
-
-    static move(piece) {
-        piece.setPlayer(this.data);
     }
 }
-
-
 
 class BoardPiece {
 
-    /**
-     * 
-     * @param {*} id ID of the piece
-     * @param {Player} p player object that can interact with this piece
-     * @param {Player} c computer object that can interact with this boardpiece
-     */
-    constructor(id, p, c) {
-
+    constructor(id) {
         this.root = document.createElement("div");
+
         this.root.id = id;
 
+        this.root.style.border = "1px solid black";
+
         
 
-        this.player = p;
-        this.computer = c;
-
-        this.control = undefined;
-        
-
-        this.root.addEventListener(() => {GameBoard.move(this, this.player, this.computer)});
-
-        /*
-        set the state of the root object
-         */
-        
-        this.root.style.borderWidth = "1pt";
-        this.root.style.borderStyle = "solid";
-        this.root.style.borderColor = "black";
-
-        this.root.style.fontSize = "30pt";
-        this.root.style.alignContent = "center";
-        this.root.style.justifyContent = "center";
-        //this.root.addEventListener("click", () => {this.setPlayer(this.player)});
-
-        document.getElementById("game").appendChild(this.root);
     }
 
-    updateUI() {
-        this.root.innerHTML = this.player;
+    setData(d) {
+        this.root.innerHTML = d;
     }
-
-    /**
-     * 
-     * @param {String} p string for the player
-     */
-    setPlayer(p) {
-        this.data = p;
-    }
-
-    /**
-     * clear function
-     */
-    clear() {
-        this.player = undefined;
-        this.root.innerHTML = "";
-    }
-
-    
 }
 
+class GameController {
 
-class GameBoard {
-
-    constructor() {
-
-
-        this.COMPUTER = new Player(playerCharacter);
-        this.PLAYER = new Player(computerCharacter);
+    constructor(player, computer) {
+        this.player = player;
+        this.computer = computer;
         
-        
-        // rows and columns represented as two arrays
-        this.rows = [];
-        this.columns = [];
-
-        let piece;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                
-                piece = new BoardPiece("r-" + (i + 1) + "-c-" + (j + 1), 
-                PLAYER, COMPUTER);
-                this.columns[j] = piece;
-                
-
-                
-                
-                this.columns[j];
-            }   
-            this.rows[i] = this.columns;
-            this.columns = [];
-        }
-        this.computer = new ComputerPlayer();
     }
 
     /**
      * 
-     * @param {BoardPiece} piece 
-     * @param {String} char 
-     * @param {EventTarget}
+     * @param {*} e 
+     * @param {*} board board array of items
      */
-    static move(BoardPiece, player, computer) {
-        // trys to move both player and computer
+    move(board) {
+        // this.player.move(e.target);
 
-        let randRow = Math.random() * 3;
-        let randCol = Math.random() * 3;
+        let random = Math.random() * 9;
 
-        let piece = this.rows[randRow][randCol];
-        
-        player.move(BoardPiece);
-        computer.move(piece);
+        board[random].root.innerHTML = this.computer.playerChar;
+
     }
 
 
-    checkWinner() {
-        for (let i = 0; i < this.rows.length; i++) {
-            this.#checkSide(i);
+}
+
+class GameBoard {
+    
+    constructor(mainPlayer) {
+
+        this.parent = document.getElementById("game");
+
+        this.items = [];
+
+
+        this.player = mainPlayer;
+        this.computer = player(undefined);
+
+        if (this.player.playerChar == 'x') {
+            this.computer.setPlayer('o');
         }
+        else {
+            this.computer.setPlayer('x');
+        }
+
+        this.controller = new GameController(this.player, this.computer);
+
+        this.createGrid();
+        this.addListeners();
+        
+
     }
+
+    createGrid() {
+
+        let piece;
+        for (let i = 0; i < 9; i++) {
+
+            piece = new BoardPiece("piece-" + i);
+            this.items[this.items.length] = piece;
+            this.parent.appendChild(piece.root);
+        }
 
     
-    #checkSide(j) {
-        let countPlayer = 0;
-        let countComputer = 0;
+    }
+
+    addListeners() {
+        let p;
+        for (let i = 0; i < this.items.length; i++) {
+            p = this.items[i];
+
+            console.log("item added");
+            p.root.addEventListener('click', this.pieceListener);
+        }
+    }
+
+    pieceListener(e) {
         
-        for (let p in this.rows[j]) {
-            if (p.player == this.player) {
-                countPlayer++;
-            }
-            else if (p.player == this.computer.token) {
-                 countComputer++;
-            }
-            else {
-                break;
-            }
-            
-        }
-    }
-    #checkCol() {
-        let countPlayer = 0;
-        let countComputer = 0;
-        for (let c in rows[j]) {
-            for (let p in c) {
-                if (p.player == this.player) {
-                    countPlayer++;
-                }
-               if (false) {
+        e.target.innerHTML = this.player.playerChar;
 
-                }
-            }
-        }
-    }
-    #checkDiagonal(i, j) {
-
+        this.controller.move(this.items);
     }
 
-    
 }
 
 class Runner {
+
     constructor() {
 
+        this.player = player(undefined);
+        
+        document.getElementById("x-character").addEventListener('click', (e) => {
+            this.player.setPlayer(e.target.innerHTML);
+        });
+        document.getElementById("o-character").addEventListener('click', (e) => {
+            this.player.setPlayer(e.target.innerHTML);
+        });
+
+        document.getElementById("start-button").addEventListener('click', () => {this.run()});
     }
 
-    main() {
-        
+    run() {
+        let game = new GameBoard(this.player);
     }
 }
+
+let runner = new Runner();
